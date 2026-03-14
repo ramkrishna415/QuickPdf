38261962b65540690a54e54a71b898336cc252fb
Ramkrishna Upadhyay <goluupadhyay600@gmail.com>
2026-03-14 17:25:17 +0530

    Updated server.js for Render deployment fixes

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Backend working 🚀");
-});
+
 
 app.use("/api", convertRoutes)
 
-app.listen(process.env.PORT, () => {
+const port =process.env.PORT ||5000
+
+app.listen(port, () => {
   console.log("Server running on port " + process.env.PORT)
 })
