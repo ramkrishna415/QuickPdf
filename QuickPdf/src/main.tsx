7dce1a7edbd2fb2a193bd72a11ca17845b43e67d
Ramkrishna Upadhyay <goluupadhyay600@gmail.com>
2026-03-14 18:34:26 +0530

    Updated server.js for Render deployment fixes

--- QuickPdf/src/main.tsx
+++ QuickPdf/src/main.tsx
@@ -1,15 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
- <React.StrictMode>
-<BrowserRouter>
-    <App />
-  </BrowserRouter>
+// createRoot(document.getElementById('root')!).render(
+//  <React.StrictMode>
+// <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+//   </React.StrictMode>
+// )
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
